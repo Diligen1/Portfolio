4672ecc78c1bd3d357f6f535db12b671cd721f83
Saian <kuba.2000.0505.kg@gmail.com>
2024-04-27 19:40:50 +0600

    19:40

--- src/components/promo/Promo.tsx
+++ src/components/promo/Promo.tsx
@@ -5,6 +5,7 @@ import image from "../../../public/assets/image/ava.jpg";
 import ButtonPromo from "../common/BTNworks/BTNworks";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Promo() {
   const [text] = useTypewriter({
@@ -17,11 +18,14 @@ export default function Promo() {
   return (
     <main className="mx-auto">
       <div className="w-full h-[700px] flex flex-row ">
-        <div className={` w-[50%] `}>
+        <div className={`w-[50%]`}>
           <div
             className={`${style.block_text} text-right h-[80%] flex flex-col justify-center gap-[15px]  px-[20px]`}
           >
-            <h3
+            <motion.h3
+              initial={{ x: -700 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 0.4 }}
               style={{ fontFamily: "Stark" }}
               className={`${style.developer} cursor-default text-[24px] tracking-[10px]`}
             >
@@ -29,8 +33,11 @@ export default function Promo() {
               <span>
                 <Cursor cursorStyle="<" cursorColor="var(--Thrid-color)" />
               </span>
-            </h3>
-            <h2
+            </motion.h3>
+            <motion.h2
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
               style={{ fontFamily: "Stark" }}
               className={`${style.title} cursor-default  text-[96px] tracking-[12px]`}
             >
@@ -39,26 +46,38 @@ export default function Promo() {
               <span className={`${style.text_kuba} pt-[13px] pb-[14px]`}>
                 KUBA
               </span>
-            </h2>
-            <p
+            </motion.h2>
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1.5 }}
               style={{ fontFamily: "Acline" }}
               className={`${style.text_text} cursor-default  text-[20px] tracking-[4px]`}
             >
               Опытный фронтенд-разработчик с обширным опытом работы в индустрии
               веб-технологий.
-            </p>
+            </motion.p>
           </div>
-          <div className="flex items-end justify-end pr-[20px] ">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 2 }}
+            className="flex items-end justify-end pr-[20px] "
+          >
             <ButtonPromo />
-          </div>
+          </motion.div>
         </div>
-        <div className={`w-[50%] flex justify-center items-center`}>
+        <motion.div
+          initial={{ x: 1000 }}
+          animate={{ x: 0 }}
+          className={`w-[50%] flex justify-center items-center`}
+        >
           <div
             className={`${style.image} relative w-[450px] h-[450px] rounded-[50%] overflow-hidden border-[5px]`}
           >
             <Image src={image} alt="ava" priority />
           </div>
-        </div>
+        </motion.div>
       </div>
     </main>
   );
